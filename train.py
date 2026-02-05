37b76663af36045369908c44e2684454d8fd4391
jtran2509 <jadetran2509@gmail.com>
2026-02-05 17:32:55 -0500

    add neural network structure and train.py

---
+++ train.py
@@ -0,0 +1,10 @@
+# What to do for this
+"""
+- Loads data (via `dataset.py`)
+- Applies augmentation (training only)
+- Instantiate the model (from cnn.py)
+- Define loss + optimizer
+- Runs the training loop
+- Save models weights
+- Logs metrics
+"""
