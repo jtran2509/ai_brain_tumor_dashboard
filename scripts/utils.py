707f66fdc86000d975bd4dccba43d21872a8471c
jtran2509 <jadetran2509@gmail.com>
2026-03-11 14:42:47 -0400

    fix utils file

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,6 +7,7 @@ import torch
 
 
 # Import dependencies necessary
+import pytorch_grad_cam
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
