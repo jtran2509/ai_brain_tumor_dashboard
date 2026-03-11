8c20d9409dc4bb27561d0f81ae17f2032e5c8650
jtran2509 <jadetran2509@gmail.com>
2026-03-11 13:48:53 -0400

    fix running error

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,7 +2,11 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-import cv2
+try: 
+  import cv2
+except ImportError:
+  import PIL.Image as Image
+  
 import torch
 
 # Import dependencies necessary
